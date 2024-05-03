1ac53228d3305d2b95a74f43b99100b584ba694f
ankit9650 <ankitprabhaker9650@gmail.com>
2024-05-03 21:01:08 +0530

    added div

--- src/components/gridimg/Gridimg.jsx
+++ src/components/gridimg/Gridimg.jsx
@@ -1,16 +1,13 @@
 import React from 'react'
 
-function Gridimg() {
+function Gridimg({ imageUrl, text }) {
   return (
-    <div className='w-full h-full ml-32 mt-8'>
-        <div className='inline-flex space-x-7'>
-            <h1 className='text-white'>GRID1</h1>        
-        </div>
-        <div className='inline-flex space-x-7'>
-            <h1 className='text-white'>GRID1</h1>        
-        </div>
-        
+    <>
+      <div className="flex flex-col relative justify-center w-3/6 h-3/6 border border-slate-700 rounded-md m-1 mt-8 p-3">
+      <img src={imageUrl} alt="Square" className="  items-center justify-center" />
+      <p className='  absolute bottom-32 left-1/4 text-3xl font-sans font-semibold  text-white'>{text}</p>
     </div>
+    </>
   )
 }
 
